6dd77e34e0ada4ab7f5cfada5840a9d1b230c59f
Jerry Xst <JerryXst@users.noreply.github.com>
2023-05-06 16:24:55 +0800

    语法错误

--- Sources/ChatGPTSwift/ChatGPTAPI.swift
+++ Sources/ChatGPTSwift/ChatGPTAPI.swift
@@ -80,7 +80,7 @@ public class ChatGPTAPI: @unchecked Sendable {
         let request = PlainTextRequest(model: model, 
                                        temperature: temperature,
                                        messages: text,
-                                       stream: stream）
+                                       stream: stream)
         return try JSONEncoder().encode(request)
     }
     
